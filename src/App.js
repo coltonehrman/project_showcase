b990703885c48da698e2a8fabc2d4376d10aa6cd
Colton Ehrman <cehrman@Coltons-MacBook-Pro.local>
2020-04-22 00:51:49 -0700

    added router to route between pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Dashboard from './components/Dashboard'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <NavBar />
-        <Dashboard />
+    <Router>
+      <div className="App">
+        <div className="container">
+          <NavBar />
+
+          <Route path="/" exact>
+            <Dashboard />
+          </Route>
+
+          <Route path="/react/components" exact>
+            <Dashboard />
+          </Route>
+        </div>
       </div>
-    </div>
+    </Router>
   )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,47 @@
 import React from 'react'
+import {
+  Link,
+  useLocation
+} from 'react-router-dom'
 import NavHeader from './NavHeader'
 import NavLink from './NavLink'
 import './NavBar.css'
 
 const NavBar = () => {
+  const { pathname: path } = useLocation()
+
   return (
     <div className="navbar">
       <NavHeader title="Project Name" />
       
       <div className="navbar--links">
-        <NavLink text="Dashboard" active />
-        <NavLink text="Timeline" />
-        <NavLink text="UML" />
-        <NavLink text="Tasks" />
-        <NavLink text="Statistics" />
-        <NavLink text="Settings" />
+        <Link to="/">
+          <NavLink text="Dashboard" active={path === '/'} />
+        </Link>
+
+        <Link to="/react/components">
+          <NavLink text="React Components" active={path === '/react/components'} />
+        </Link>
+
+        <Link to="/timeline">
+          <NavLink text="Timeline" active={path === '/timeline'} />
+        </Link>
+
+        <Link to="/uml">
+          <NavLink text="UML" active={path === '/uml'} />
+        </Link>
+
+        <Link to="/tasks">
+          <NavLink text="Tasks" active={path === '/tasks'} />
+        </Link>
+
+        <Link to="/statistics">
+          <NavLink text="Statistics" active={path === '/statistics'} />
+        </Link>
+
+        <Link to="/settings">
+          <NavLink text="Settings" active={path === '/settings'} />
+        </Link>
       </div>
     </div>
   )
