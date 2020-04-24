57b6a018149e7edaa1af9b713d7809c144d38777
Colton Ehrman <cehrman@Coltons-MacBook-Pro.local>
2020-04-24 15:06:13 -0700

    create new components for tech section

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,73 +1,14 @@
 import React, { useState } from 'react'
+import TechSection from './TechSection'
 import Section from './Section'
 import Card from './Card'
 import './Dashboard.css'
-
-const noop = () => {}
 
 const Title = ({ children }) => (
   <div className="dashboard--header-title">
     <h3 className="dashboard--header-title__h4">{children}</h3>
   </div>
 )
-
-const TechTab = ({
-  color,
-  styles = {},
-  first = false,
-  last = false,
-  editing = false,
-  onChange = noop,
-  setValue = noop,
-  setEditing = noop,
-  onClick = false,
-  children
-}) => {
-  let _styles = {
-    ...styles
-  }
-
-  if (color) {
-    _styles.backgroundColor = color
-  }
-
-  if (first) {
-    _styles.marginLeft = '0'
-  }
-
-  if (last) {
-    _styles.marginRight = '0'
-  }
-
-  const renderBody = () => {
-    if (!editing)
-      return children
-    return (
-      <input
-        type="text"
-        value={children}
-        className="dashboard--techtab__edit"
-        style={{
-          width: `${children.length * 10}px`
-        }}
-        onChange={(e) => onChange(e.target.value)}
-        onKeyDown={({ key }) => (key === 'Enter' || key === 'Return') && setValue()}
-        onBlur={setValue}
-        autoFocus
-      />
-    )
-  }
-
-  return (
-    <div
-      className="dashboard--techtab"
-      style={_styles}
-      onClick={onClick || setEditing}
-    >
-      {renderBody()}
-    </div>
-  )
-}
 
 const SocialLink = ({
   url,
@@ -91,38 +32,6 @@ const SocialLink = ({
 }
 
 const Dashboard = () => {
-  const [tech, setTech] = useState(JSON.parse(
-    localStorage.getItem('technologies') || '[]'
-  ))
-
-  const [editing, setEditing] = useState(false)
-
-  const addTech = () => {
-    setTech([...tech, { text: '', editing: true }])
-    setEditing(true)
-  }
-
-  const editTech = (i) => (newTech) => {
-    tech[i].text = newTech
-    setTech([...tech])
-  }
-
-  const setEditingFor = (i) => () => {
-    tech[i].editing = true
-    setEditing(true)
-  }
-
-  const finishEditingTech = (i) => () => {
-    if (tech[i].text.trim() === '')
-      tech.splice(i, 1)
-    else
-      tech[i].editing = false
-
-    setTech([...tech])
-    setEditing(false)
-    localStorage.setItem('technologies', JSON.stringify(tech))
-  }
-
   return (
     <div className="dashboard">
       <div className="dashboard--header">
@@ -135,40 +44,13 @@ const Dashboard = () => {
         </div>
       </div>
       
-      <Section title="Technologies Used">
-        {tech && tech.map((t, i) => (
-          <TechTab
-            key={i}
-            first={i === 0}
-            color="#705CF1"
-            editing={t.editing}
-            onChange={editTech(i)}
-            setEditing={setEditingFor(i)}
-            setValue={finishEditingTech(i)}
-          >
-            {t.text}
-          </TechTab>
-        ))}
-
-        {!editing && <TechTab
-          last
-          color="transparent"
-          styles={{
-            color: '#705CF1',
-            paddingLeft: '20px',
-            paddingRight: '20px',
-            border: '2px solid #705CF1',
-            cursor: 'pointer'
-          }}
-          onClick={() => addTech()}
-        >
-          +
-        </TechTab>}
-      </Section>
+      <TechSection />
 
       <Section title="Project Description">
         <Card>
-          <h4 className="dashboard--description">
+          <h4
+            className="dashboard--description"
+          >
             <p>
               A project planner for beginnners to help them plan out projects step by step.
               Helps junior developers start projects from sratch, with all things in mind before they even start coding.
---
+++ src/components/TechSection.js
@@ -0,0 +1,131 @@
+import React, { useState } from 'react'
+import Section from './Section'
+
+const noop = () => {}
+
+const TechTab = ({
+  color,
+  styles = {},
+  first = false,
+  last = false,
+  editing = false,
+  onChange = noop,
+  setValue = noop,
+  setEditing = noop,
+  onClick = false,
+  children
+}) => {
+  let _styles = {
+    ...styles
+  }
+
+  if (color) {
+    _styles.backgroundColor = color
+  }
+
+  if (first) {
+    _styles.marginLeft = '0'
+  }
+
+  if (last) {
+    _styles.marginRight = '0'
+  }
+
+  const renderBody = () => {
+    if (!editing)
+      return children
+    return (
+      <input
+        type="text"
+        value={children}
+        className="dashboard--techtab__edit"
+        style={{
+          width: `${children.length * 10}px`
+        }}
+        onChange={(e) => onChange(e.target.value)}
+        onKeyDown={({ key }) => (key === 'Enter' || key === 'Return') && setValue()}
+        onBlur={setValue}
+        autoFocus
+      />
+    )
+  }
+
+  return (
+    <div
+      className="dashboard--techtab"
+      style={_styles}
+      onClick={onClick || setEditing}
+    >
+      {renderBody()}
+    </div>
+  )
+}
+
+const TechSection = () => {
+  const [tech, setTech] = useState(JSON.parse(
+    localStorage.getItem('technologies') || '[]'
+  ))
+
+  const [editing, setEditing] = useState(false)
+
+  const addTech = () => {
+    setTech([...tech, { text: '', editing: true }])
+    setEditing(true)
+  }
+
+  const editTech = (i) => (newTech) => {
+    tech[i].text = newTech
+    setTech([...tech])
+  }
+
+  const setEditingFor = (i) => () => {
+    tech[i].editing = true
+    setEditing(true)
+  }
+
+  const finishEditingTech = (i) => () => {
+    if (tech[i].text.trim() === '')
+      tech.splice(i, 1)
+    else
+      tech[i].editing = false
+
+    setTech([...tech])
+    setEditing(false)
+    localStorage.setItem('technologies', JSON.stringify(tech))
+  }
+
+  return (
+    <Section title="Technologies Used">
+      {tech && tech.map((t, i) => (
+        <TechTab
+          key={i}
+          first={i === 0}
+          color="#705CF1"
+          editing={t.editing}
+          onChange={editTech(i)}
+          setEditing={setEditingFor(i)}
+          setValue={finishEditingTech(i)}
+        >
+          {t.text}
+        </TechTab>
+      ))}
+
+      {!editing && <TechTab
+        last
+        color="transparent"
+        styles={{
+          color: '#705CF1',
+          paddingLeft: '20px',
+          paddingRight: '20px',
+          border: '2px solid #705CF1',
+          cursor: 'pointer'
+        }}
+        onClick={() => addTech()}
+      >
+        +
+      </TechTab>}
+    </Section>
+  )
+}
+
+export default TechSection
