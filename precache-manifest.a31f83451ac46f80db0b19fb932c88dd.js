321b91619fce0ff78e884f098e1a41a9cd800859
Colton Ehrman <cehrman@Coltons-MacBook-Pro.local>
2020-04-24 23:39:09 -0700

    Updates

---
+++ precache-manifest.a31f83451ac46f80db0b19fb932c88dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8fb8487ca63cd9f43bb25d5e2b4840f",
+    "url": "/project_showcase-planner/index.html"
+  },
+  {
+    "revision": "f8e8b97c10d32b2ac307",
+    "url": "/project_showcase-planner/static/css/main.82dd3127.chunk.css"
+  },
+  {
+    "revision": "ca1bd1e94277b5a38635",
+    "url": "/project_showcase-planner/static/js/2.b9241ab2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project_showcase-planner/static/js/2.b9241ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e8b97c10d32b2ac307",
+    "url": "/project_showcase-planner/static/js/main.7f5fcd20.chunk.js"
+  },
+  {
+    "revision": "0e2dbfa3726e5200c86a",
+    "url": "/project_showcase-planner/static/js/runtime-main.6e5f791c.js"
+  }
+]);
