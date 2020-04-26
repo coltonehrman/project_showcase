26f8a40b59c4390995be4936f48768af392f66ca
Colton Ehrman <cehrman@Coltons-MacBook-Pro.local>
2020-04-26 08:05:36 -0700

    Updates

--- precache-manifest.0529bf9b03805f46cf40f07d85878c01.js
+++ precache-manifest.0529bf9b03805f46cf40f07d85878c01.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "75f3ebfa474318e66b1f1ffcea5633be",
+    "revision": "d08da09d7d7086b6811618d6204f6c08",
     "url": "/project_showcase-planner/index.html"
   },
   {
-    "revision": "2f215575c4e0c976dfbb",
+    "revision": "d1f15c4a06d022fceb09",
     "url": "/project_showcase-planner/static/css/main.3d455426.chunk.css"
   },
   {
-    "revision": "62475048b7e0184601c4",
-    "url": "/project_showcase-planner/static/js/2.37d7f710.chunk.js"
+    "revision": "4e7af92dd6c368f11335",
+    "url": "/project_showcase-planner/static/js/2.45d7cff3.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/project_showcase-planner/static/js/2.37d7f710.chunk.js.LICENSE.txt"
+    "url": "/project_showcase-planner/static/js/2.45d7cff3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2f215575c4e0c976dfbb",
-    "url": "/project_showcase-planner/static/js/main.85307d06.chunk.js"
+    "revision": "d1f15c4a06d022fceb09",
+    "url": "/project_showcase-planner/static/js/main.cf3707ea.chunk.js"
   },
   {
     "revision": "0e2dbfa3726e5200c86a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project_showcase-planner/precache-manifest.baa5579d53cf6fa43e7307083cf92d35.js"
+  "/project_showcase-planner/precache-manifest.0529bf9b03805f46cf40f07d85878c01.js"
 );
 
 self.addEventListener('message', (event) => {
