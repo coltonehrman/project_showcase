c57936cad72012bc31c82266b30f235888468472
Colton Ehrman <coltonje95@gmail.com>
2020-05-06 07:49:29 -0700

    remove stuff thats not being used

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -122,24 +122,6 @@ const Dashboard = () => {
           <img className="image" key={i} src={image} />
         ))}
       </Section>
-
-      {/* <Section title="Drawings">
-        <a href="https://i.ibb.co/hMNSRKW/IMG-1310.jpg" target="_blank" rel="noopener noreferrer">
-          <img className="drawing-image" alt="Drawings-1" src="https://i.ibb.co/hMNSRKW/IMG-1310.jpg" />
-        </a>
-
-        <a href="https://i.ibb.co/nwtsP3d/IMG-1308.jpg" target="_blank" rel="noopener noreferrer">
-          <img className="drawing-image" alt="Drawings-2" src="https://i.ibb.co/nwtsP3d/IMG-1308.jpg" />
-        </a>
-
-        <a href="https://i.ibb.co/M1mMxnQ/IMG-1311.jpg" target="_blank" rel="noopener noreferrer">
-          <img className="drawing-image" alt="Drawings-3" src="https://i.ibb.co/M1mMxnQ/IMG-1311.jpg" />
-        </a>
-
-        <a href="https://i.ibb.co/SrKxCGc/IMG-1309.jpg" target="_blank" rel="noopener noreferrer">
-          <img className="drawing-image" alt="Drawings-4" src="https://i.ibb.co/SrKxCGc/IMG-1309.jpg" />
-        </a>
-      </Section> */}
     </div>
   )
 }
